e8152f51f272ee16928d60377e059d92571073e0
Joaquin CR <j.rodriguez@blueparrotsoftwarellc.com>
2023-06-05 12:08:20 -0600

    Finish Lesson 2

---
+++ LESSON02/build/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+let mayName = 'Joaquin';
+let meaningOfLife;
+let isLoading;
+let album;
+mayName = 'Joaquin CR';
+meaningOfLife = 22;
+isLoading = true;
+album = 2001;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /|w+/g;
---
+++ LESSON02/src/main.ts
@@ -0,0 +1,18 @@
+let mayName: string = 'Joaquin';
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+
+mayName = 'Joaquin CR';
+meaningOfLife = 22;
+isLoading = true;
+album = 2001;
+
+const sum = (a: number, b: number) => {
+    return a + b;
+}
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /|w+/g
