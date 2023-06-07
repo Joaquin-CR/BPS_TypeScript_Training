f040d570d04e5cb81aa333371de9c87148b82ccc
Joaquin CR <j.rodriguez@blueparrotsoftwarellc.com>
2023-06-07 12:52:08 -0600

    Lesson 17: Project 2 - Modified file App.tsx

--- LESSON17/src/App.tsx
+++ LESSON17/src/App.tsx
@@ -1,13 +1,23 @@
+import { useState } from "react"
+import Cart from "./components/Cart"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import ProductList from "./components/ProductList"
 
-function App() {  
 
-  return (
+function App() {
+  const [viewCart, setViewCart] = useState<boolean>(false);
+
+  const pageContent = viewCart ? <Cart /> : <ProductList />;
+
+  const content = (
     <>
-      <div className='App'>
-        {/*  */}
-      </div>
+      <Header viewCart={viewCart} setViewCArt={setViewCart}/>
+      {pageContent}
+      <Footer viewCart={viewCart}/>
     </>
   )
+  return content;
 }
 
 export default App
