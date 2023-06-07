a24abf6ce115a468a86f25922fd4281aaa0ef9f8
Joaquin CR <j.rodriguez@blueparrotsoftwarellc.com>
2023-06-07 14:17:40 -0600

    Modified the components Footer, Header and ProductList

--- LESSON17/src/components/Header.tsx
+++ LESSON17/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Nav from './Nav';
+import useCart from '../hooks/useCart';
 
 type PropsType = {
     viewCart: boolean,
@@ -7,13 +8,16 @@ type PropsType = {
 }
 
 const Header = ({ viewCart, setViewCart }: PropsType) => {
-    const  content = (
+
+    const { totalItems, totalPrice } = useCart();
+
+    const content = (
         <header className='header'>
             <div className="headder_title-bar">
                 <h1>Acme CO.</h1>
                 <div className="header_price-box">
-                    <p>Total Items:</p>
-                    <p>Total Price:</p>
+                    <p>Total Items: {totalItems}</p>
+                    <p>Total Price: {totalPrice}</p>
                 </div>
             </div>
             <Nav viewCart={viewCart} setViewCart={setViewCart}></Nav>
